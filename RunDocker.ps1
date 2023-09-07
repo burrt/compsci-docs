39d29044c7b415f6ab15f1f31d2f4715c2f497ad
Geoffrey Choy <geoffchoy@outlook.com>
2023-09-07 15:22:38 +1000

    Add Docker image tag for powershell

--- RunDocker.ps1
+++ RunDocker.ps1
@@ -1,6 +1,6 @@
 docker run `
     --rm `
-    --label=jekyll `
+    --label=jekyll/jekyll:3.8 `
     --volume=${pwd}:/srv/jekyll `
     -it -p 4000:4000 jekyll/jekyll `
     jekyll serve --force_polling
